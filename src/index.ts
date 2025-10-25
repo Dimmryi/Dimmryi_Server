d470099528cd627fb88cc0e111e6d2c5d94902de
Dimmryi <dimmryi@gmail.com>
2025-10-25 18:36:20 +0300

    fix listen on 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -688,5 +688,5 @@ app.use(CommentsRoutes);
 
 //Server
 app.listen(port,  '0.0.0.0',() => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://0.0.0.0:${port}`);
 });
