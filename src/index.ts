f83da73808166f6c886374d30802a540e9d01c3d
Dimmryi <dimmryi@gmail.com>
2026-04-21 09:49:54 +0300

    update the variable: publicId

--- src/index.ts
+++ src/index.ts
@@ -381,7 +381,7 @@ app.get('/api/ad', async (req, res) => {
 app.delete('/api/ads/:publicId', async (req:any, res:any) => {
     try{
         const { publicId } = req.params;
-        const deletedVideoUrl = await AdsModel.deleteMany({ publicId : publicId });
+        const deletedVideoUrl = await AdsModel.deleteMany({ publicId });
         if (deletedVideoUrl.deletedCount === 0) {
             return res.status(404).json({ message: 'No Ads data found.' });
         }
