960c7be23a1a8e1746f9a701831ed688a64e99fc
Dimmryi <dimmryi@gmail.com>
2026-05-19 18:13:10 +0300

    added httpServer.listen(port, '0.0.0.0' - hostname

--- src/index.ts
+++ src/index.ts
@@ -938,8 +938,11 @@ const io = new Server(httpServer, {
 
 setupChatSocket(io);
 
+httpServer.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+});
 
-// // Server
+// // Server old
 // app.listen(port,  '0.0.0.0',() => {
 //     console.log(`Server running on http://0.0.0.0:${port}`);
 // });
